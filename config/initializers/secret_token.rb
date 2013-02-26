698ad25c5a64e519bd4b617b10456b949df0b372
eakmotion <eakpun@gmail.com>
2013-02-26 19:08:41 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamescore::Application.config.secret_token = '09b4df961987020adab2c13875964a02b3a71482abb81558d524dc6b5a0833214a97f4814da437bd935ab5ac7169f23385358caf34b0c188c21ab11989de2653'
